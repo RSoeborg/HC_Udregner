63bad91893b49ca90cf9874a77e1f8b58a062bbe
Rasmus <rsoeborg@gmail.com>
2020-02-26 14:27:58 +0100

    Methods to select & show maple path.

--- HC_Udregner/MainWindow.xaml.cs
+++ HC_Udregner/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using HC_Udregner.Properties;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +26,38 @@ namespace HC_Udregner
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SelectMaple()
+        {
+            var ofd = new OpenFileDialog();
+            var result = ofd.ShowDialog();
+            if (result.HasValue && result.Value)
+            {
+                if (ofd.FileName.Contains("maplew.exe") || ofd.FileName.Contains("cmaple.exe"))
+                {
+                    var file = new FileInfo(ofd.FileName);
+                    var cmaple = file.Name.Equals("cmaple.exe") ? file : file.Directory.GetFiles().FirstOrDefault(f => f.Name.Equals("cmaple.exe"));
+
+                    if (cmaple != default(FileInfo))
+                    {
+                        Settings.Default.Path = cmaple.FullName;
+                        Settings.Default.Save();
+                        Settings.Default.Reload();
+                        UpdateMaplePath();
+                        return;
+                    }
+                }
+                MessageBox.Show("Maple Command Line kunne ikke findes.");
+            }
+        }
+        private void UpdateMaplePath()
+        {
+            if (!string.IsNullOrWhiteSpace(Settings.Default.Path))
+            {
+                FileInfo cmaple = new FileInfo(Settings.Default.Path);
+                //lblMaplePath.Content = $"Path: {cmaple.Directory.Parent.FullName}";
+            }
         }
     }
 }
